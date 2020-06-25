3b3c276a433e02b07abde3baab38419fa613e254
David Craven <david@craven.ch>
2020-06-25 02:01:24 +0200

    Add faucet.

---
+++ faucet/src/mock.rs
@@ -0,0 +1,73 @@
+use crate::{Module, Trait};
+use frame_support::{impl_outer_origin, parameter_types, weights::Weight};
+use frame_system as system;
+use pallet_balances as balances;
+use sp_core::H256;
+use sp_runtime::{
+    testing::Header,
+    traits::{BlakeTwo256, IdentityLookup},
+    Perbill,
+};
+
+impl_outer_origin! {
+    pub enum Origin for Test {}
+}
+
+#[derive(Clone, Eq, PartialEq)]
+pub struct Test;
+
+parameter_types! {
+    pub const BlockHashCount: u64 = 250;
+    pub const MaximumBlockWeight: Weight = 1024;
+    pub const MaximumBlockLength: u32 = 2 * 1024;
+    pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+}
+impl system::Trait for Test {
+    type Origin = Origin;
+    type Call = ();
+    type Index = u64;
+    type BlockNumber = u64;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = u64;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Header = Header;
+    type Event = ();
+    type BlockHashCount = BlockHashCount;
+    type MaximumBlockWeight = MaximumBlockWeight;
+    type DbWeight = ();
+    type BlockExecutionWeight = ();
+    type ExtrinsicBaseWeight = ();
+    type MaximumExtrinsicWeight = MaximumBlockWeight;
+    type MaximumBlockLength = MaximumBlockLength;
+    type AvailableBlockRatio = AvailableBlockRatio;
+    type Version = ();
+    type ModuleToIndex = ();
+    type AccountData = balances::AccountData<u128>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+}
+parameter_types! {
+    pub const ExistentialDeposit: u128 = 500;
+}
+impl balances::Trait for Test {
+    type Balance = u128;
+    type Event = ();
+    type ExistentialDeposit = ExistentialDeposit;
+    type DustRemoval = ();
+    type AccountStore = system::Module<Test>;
+}
+pub const MINT_UNIT: u128 = 10_000_000;
+pub type AccountStore = <Test as balances::Trait>::AccountStore;
+impl Trait for Test {
+    const MINT_UNIT: Self::Balance = MINT_UNIT;
+}
+pub type FaucetModule = Module<Test>;
+pub type BalancesModule = balances::Module<Test>;
+
+pub fn new_test_ext() -> sp_io::TestExternalities {
+    system::GenesisConfig::default()
+        .build_storage::<Test>()
+        .unwrap()
+        .into()
+}
