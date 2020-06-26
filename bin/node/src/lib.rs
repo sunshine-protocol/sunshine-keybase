828252c8a8177b2800d3331104cb1e7b981d2867
David Craven <david@craven.ch>
2020-06-27 00:21:00 +0200

    Refactor node.

--- bin/node/src/lib.rs
+++ bin/node/src/lib.rs
@@ -1,2 +1,10 @@
 pub mod chain_spec;
 pub mod service;
+
+pub const IMPL_NAME: &str = "Sunshine Identity Test Node";
+pub const IMPL_VERSION: &str = env!("CARGO_PKG_VERSION");
+pub const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+pub const AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
+pub const SUPPORT_URL: &str = env!("CARGO_PKG_HOMEPAGE");
+pub const COPYRIGHT_START_YEAR: i32 = 2020;
+pub const EXECUTABLE_NAME: &str = env!("CARGO_PKG_NAME");
--- bin/node/src/main.rs
+++ bin/node/src/main.rs
@@ -1,12 +1,69 @@
 //! Substrate Node Template CLI library.
-#![warn(missing_docs)]
 
-mod chain_spec;
-#[macro_use]
-mod service;
-mod cli;
-mod command;
+use sc_cli::{RunCmd, Subcommand, SubstrateCli};
+use structopt::StructOpt;
+use test_node::{chain_spec, new_full_start, service};
+
+#[derive(Debug, StructOpt)]
+pub struct Cli {
+    #[structopt(subcommand)]
+    pub subcommand: Option<Subcommand>,
+
+    #[structopt(flatten)]
+    pub run: RunCmd,
+}
+
+impl SubstrateCli for Cli {
+    fn impl_name() -> &'static str {
+        test_node::IMPL_NAME
+    }
+
+    fn impl_version() -> &'static str {
+        test_node::IMPL_VERSION
+    }
+
+    fn description() -> &'static str {
+        test_node::DESCRIPTION
+    }
+
+    fn author() -> &'static str {
+        test_node::AUTHOR
+    }
+
+    fn support_url() -> &'static str {
+        test_node::SUPPORT_URL
+    }
+
+    fn copyright_start_year() -> i32 {
+        test_node::COPYRIGHT_START_YEAR
+    }
+
+    fn executable_name() -> &'static str {
+        test_node::EXECUTABLE_NAME
+    }
+
+    fn load_spec(&self, id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
+        Ok(match id {
+            "dev" => Box::new(chain_spec::development_config()),
+            "" | "local" => Box::new(chain_spec::local_testnet_config()),
+            path => Box::new(chain_spec::ChainSpec::from_json_file(
+                std::path::PathBuf::from(path),
+            )?),
+        })
+    }
+}
 
 fn main() -> sc_cli::Result<()> {
-    command::run()
+    let cli = <Cli as SubstrateCli>::from_args();
+
+    match &cli.subcommand {
+        Some(subcommand) => {
+            let runner = cli.create_runner(subcommand)?;
+            runner.run_subcommand(subcommand, |config| Ok(new_full_start!(config).0))
+        }
+        None => {
+            let runner = cli.create_runner(&cli.run)?;
+            runner.run_node(service::new_light, service::new_full, test_runtime::VERSION)
+        }
+    }
 }
