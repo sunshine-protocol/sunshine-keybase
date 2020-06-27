721e558a86ab97cc02dee2491a8e5fe6eb29c6cf
Shady Khalifa <shekohex@gmail.com>
2020-06-28 00:42:01 +0200

    stop using dart-bingen and expand macros

--- identity/ffi/binding.h
+++ identity/ffi/binding.h
@@ -3,6 +3,44 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define CLIENT_ALREADY_INIT 8
+
+#define CLIENT_BAD_CSTR 2
+
+#define CLIENT_BAD_MNEMONIC 12
+
+#define CLIENT_BAD_SURI 11
+
+#define CLIENT_BAD_UID 13
+
+#define CLIENT_FAIL_TO_LOCK 14
+
+#define CLIENT_FAIL_TO_UNLOCK 16
+
+#define CLIENT_HAS_DEVICE_KEY 9
+
+#define CLIENT_IPFS_CONFIG_ERR 4
+
+#define CLIENT_IPFS_STORE_ERR 6
+
+#define CLIENT_KEYSTORE_OPEN_ERR 5
+
+#define CLIENT_LOCKED_OK 15
+
+#define CLIENT_OK 1
+
+#define CLIENT_PASSWORD_TOO_SHORT 10
+
+#define CLIENT_SUBXT_CREATE_ERR 3
+
+#define CLIENT_UNINIT 7
+
+#define CLIENT_UNKNOWN -1
+
+#define CLIENT_UNKNOWN_SERVICE 18
+
+#define CLIENT_UNLOCKED_OK 17
+
 /**
  * Add new paperkey from the current account
  */
